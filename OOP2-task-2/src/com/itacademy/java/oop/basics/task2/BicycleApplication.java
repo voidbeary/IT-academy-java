82751ee878e04f76eda686f19c6d59963cb9512c
Justina Andrulionyte <69106997+voidbeary@users.noreply.github.com>
2022-03-06 00:21:42 +0200

    feat road bike functionality

--- OOP2-task-2/src/com/itacademy/java/oop/basics/task2/BicycleApplication.java
+++ OOP2-task-2/src/com/itacademy/java/oop/basics/task2/BicycleApplication.java
@@ -3,6 +3,7 @@ package com.itacademy.java.oop.basics.task2;
 public class BicycleApplication {
     public static void main(String[] args) {
         MountainBike mountainBike = new MountainBike();
+        RoadBike roadBike = new RoadBike();
 
         try {
             mountainBike.applyBrakes();
@@ -10,9 +11,7 @@ public class BicycleApplication {
             System.out.println(e.getMessage());
             throw e;
         }
-
         System.out.println("Current mountain bike speed after brakes is " + mountainBike.speed);
-
 
         try {
             mountainBike.speedUp(20);
@@ -31,6 +30,28 @@ public class BicycleApplication {
         }
         System.out.println("Current mountain bike gear after change is " + mountainBike.gear);
 
+        try {
+            roadBike.applyBrakes();
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
+        System.out.println("Current road bike speed after brakes is " + roadBike.speed);
+        try {
+            roadBike.speedUp(5);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
+        System.out.println("Current road bike speed after speed up is " + roadBike.speed);
+
+        try {
+            roadBike.changeGear(1);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
+        System.out.println("Current road bike gear after change is " + roadBike.gear);
 
     }
 }
--- OOP2-task-2/src/com/itacademy/java/oop/basics/task2/RoadBike.java
+++ OOP2-task-2/src/com/itacademy/java/oop/basics/task2/RoadBike.java
@@ -1,21 +1,38 @@
 package com.itacademy.java.oop.basics.task2;
 
 public class RoadBike implements Bicycle {
-    int gear;
-    int speed;
+    int gear = 5;
+    int speed = 50;
+    int brakeValue = 5;
 
     @Override
     public void changeGear(int newGear) {
-
+        if (!(newGear == 1 || newGear == -1 || newGear == 2 || newGear == -2)) {
+            throw new RuntimeException("Gear can only be increased or decreased by only 1,2,-1or -2");
+        } else if (gear + newGear > 10 || gear + newGear < 0) {
+            throw new RuntimeException("Gear is either over the max 10 or below 0");
+        }
+        this.gear = this.gear + newGear;
     }
 
     @Override
     public void speedUp(int increment) {
-
+        if (increment < 0) {
+            throw new RuntimeException("Increment value can't be negative number. Please use positive numbers");
+        } else if (increment + speed > 50) {
+            int highestIncrement = 50 - speed;
+            throw new RuntimeException("Speed is too high. Highest possible increment is " + highestIncrement);
+        }
+        this.speed = this.speed + increment;
     }
 
     @Override
     public void applyBrakes() {
-
+        if (speed - brakeValue < 0) {
+            throw new RuntimeException("Road bike can't have negative speed");
+        } else if (brakeValue < 0) {
+            throw new RuntimeException("Brakes can only be applied using positive values. Please use positive values");
+        }
+        this.speed = this.speed - brakeValue;
     }
 }
